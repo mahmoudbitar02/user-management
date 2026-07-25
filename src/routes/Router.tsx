8b8351effeda690bf75a01c8c6ebe71e500151e9
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2026-07-25 23:36:56 +0200

    fix edit root

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -18,7 +18,7 @@ export const router = createBrowserRouter([
         element: <CreateUser />,
       },
       {
-        path: "edit/:id",
+        path: "/edit/:id",
         element: <UpdateUser />,
       },
     ],
